b8611378c54714819304ca5629ab16ad187009cf
nikhil2830322 <nikhilluck@outlook.com>
2026-07-10 13:35:22 -0500

    Update MonteCarlo+Backtester.py

--- src/MonteCarlo+Backtester.py
+++ src/MonteCarlo+Backtester.py
@@ -1,3 +1,11 @@
+# ==============================================================================
+#  Copyright (C) 2026 Nikhil Mangalampally
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License.
+# ==============================================================================
+
+
 import numpy as np
 import pandas as pd
 import yfinance as yf
